98b0d7012e41e5099b7a0dc155a1a1c9d45fc5ca
manigandan-d <manigandan220916@gmail.com>
2025-08-02 13:18:22 +0530

    Push button interfacing

---
+++ btn.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time 
+
+GPIO.setmode(GPIO.BCM)
+
+BTN_PIN = 18
+GPIO.setup(BTN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+try: 
+    while True:
+        if GPIO.input(BTN_PIN) == GPIO.HIGH:
+            print("Button Pressed")
+
+        else:
+            print("Button Released")
+        
+        time.sleep(0.2)
+    
+except KeyboardInterrupt:
+    print("Exiting...")
+
+finally:
+    GPIO.cleanup()
