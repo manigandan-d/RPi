9ae98b5770f0c0fe84e9a9da0b7a2f0814226b1f
manigandan-d <manigandan220916@gmail.com>
2025-08-02 13:16:25 +0530

    led interfacing

---
+++ led.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO 
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+LED_PIN = 17
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+print("Starting...")
+
+try: 
+    while True: 
+        GPIO.output(LED_PIN, GPIO.HIGH)
+        time.sleep(1)
+
+        GPIO.output(LED_PIN, GPIO.LOW)
+        time.sleep(1) 
+
+except KeyboardInterrupt: 
+    print("Exiting...")
+
+finally: 
+    GPIO.cleanup()
