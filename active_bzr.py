42e767e072d4f6e572b85ece4f9f8f627cc64628
manigandan-d <manigandan220916@gmail.com>
2025-09-27 15:33:00 +0530

    active buzzer - beep every 1 sec

---
+++ active_bzr.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+BUZZER_PIN = 18
+GPIO.setup(BUZZER_PIN, GPIO.OUT)
+
+print("Starting...")
+
+try:
+    while True:
+        GPIO.output(BUZZER_PIN, GPIO.HIGH) 
+        time.sleep(1)
+
+        GPIO.output(BUZZER_PIN, GPIO.LOW)  
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("Exiting...")
+
+finally:
+    GPIO.cleanup()
