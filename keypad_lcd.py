f4fcaa63febf7348f7be56b99fa6848d4e28bf20
manigandan-d <manigandan220916@gmail.com>
2025-10-05 13:02:45 +0530

    incorporating keypad + LCD

---
+++ keypad_lcd.py
@@ -0,0 +1,37 @@
+from keypad import Keypad 
+import time 
+from RPLCD.i2c import CharLCD
+
+KEYPAD = [
+    ['1', '2', '3', 'A'], 
+    ['4', '5', '6', 'B'], 
+    ['7', '8', '9', 'C'], 
+    ['*', '0', '#', 'D']
+]
+
+ROW_PINS = [5, 6, 13, 19]
+COL_PINS = [12, 16, 20, 21]
+
+keypad = Keypad(ROW_PINS, COL_PINS, KEYPAD)
+
+lcd = CharLCD('PCF8574', 0x27)
+lcd.clear()
+
+print("Starting...")
+
+try:
+    print("Press keys on the keypad")
+
+    while True:
+        key = keypad.get_key()    
+        if key:
+            lcd.write_string(f"Key Pressed: {key}")
+        time.sleep(0.1)
+
+
+except KeyboardInterrupt:
+    print("Exiting...")
+
+finally:
+    lcd.clear()
+    keypad.cleanup()
