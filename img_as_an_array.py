6c0fdfe6c6f77f65deb67017e2de0b4c19afc1e4
manigandan-d <manigandan220916@gmail.com>
2025-10-17 15:25:59 +0530

    seeing the image as an array

---
+++ img_as_an_array.py
@@ -0,0 +1,29 @@
+from picamera2 import Picamera2
+import cv2
+import time
+
+picam2 = Picamera2()
+
+picam2.preview_configuration.main.size = (1280, 720)  
+picam2.preview_configuration.main.format = "RGB888"   
+picam2.preview_configuration.align()                  
+picam2.configure("preview")                           
+
+picam2.start()
+time.sleep(1) 
+
+print("Press 'q' to quit")
+
+while True:
+    frame = picam2.capture_array()  
+    cv2.imshow("Raspberry Pi Camera Feed", frame)  
+
+    print("Frame shape:", frame.shape)
+    print("Data type:", frame.dtype)
+    print("Top-left pixel:", frame[0, 0])
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+picam2.stop()
+cv2.destroyAllWindows()
